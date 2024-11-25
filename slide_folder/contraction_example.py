d927337da728841ae138d125f9632ccb549922b8
JHIH-SIOU LI <jsli@linux.com>
2024-11-25 21:26:50 +0800

    feat: update contraction_example page presentation

--- slide_folder/contraction_example.py
+++ slide_folder/contraction_example.py
@@ -7,10 +7,19 @@ def contraction_example(self):
         y_range=[-3, 3, 1],
         axis_config={"color": BLUE}
     ).to_edge(DOWN)
-    labels = axes.get_axis_labels(x_label="x", y_label="y")
+    labels = axes.get_axis_labels(x_label="x_1", y_label="x_2")
 
     self.play(Create(axes), Write(labels))
     self.wait(1)
+
+    self.next_slide()
+
+    # Add a label to indicate contraction mapping
+    contraction_label = MathTex(r"f(x, y) = (\frac{1}{2}x, \frac{1}{2}y)")
+    contraction_label.to_corner(UP + LEFT)
+    self.play(Write(contraction_label))
+
+    self.next_slide()
 
     # Points before contraction mapping (9 points in grid formation)
     points = [np.array([x, y, 0]) for x in np.linspace(-2, 2, 3) for y in np.linspace(-2, 2, 3)]
@@ -21,12 +30,6 @@ def contraction_example(self):
     # Create dots for original points
     dots = [Dot(axes.c2p(point[0], point[1]), color=YELLOW) for point in points]
     self.play(*[Create(dot) for dot in dots])
-    self.wait(1)
-
-    # Highlight the fixed point (0, 0)
-    fixed_point = Dot(axes.c2p(0, 0), color=BLUE).scale(1.5)
-    fixed_point_label = Text("Fixed Point", font_size=24).next_to(fixed_point, UP)
-    self.play(Create(fixed_point), Write(fixed_point_label))
     self.wait(1)
 
     # Create lines connecting original points in grid fashion
@@ -40,9 +43,15 @@ def contraction_example(self):
     self.play(*[Create(line) for line in original_lines])
     self.wait(1)
 
+    self.next_slide()
+
+    # Animate the transformation from original to contracted points
+    arrows = [Arrow(axes.c2p(point[0], point[1]), axes.c2p(contracted_point[0], contracted_point[1]), buff=0.1, color=WHITE)
+              for point, contracted_point in zip(points, contracted_points)]
+    self.next_slide()
+
     # Create dots for contracted points
     contracted_dots = [Dot(axes.c2p(contracted_point[0], contracted_point[1]), color=RED) for contracted_point in contracted_points]
-
     # Create lines connecting contracted points in grid fashion
     contracted_lines = []
     for i in range(3):
@@ -50,26 +59,28 @@ def contraction_example(self):
         contracted_lines.append(Line(axes.c2p(-1, -1 + i, 0), axes.c2p(1, -1 + i, 0), color=PURPLE))
         # Vertical lines
         contracted_lines.append(Line(axes.c2p(-1 + i, -1, 0), axes.c2p(-1 + i, 1, 0), color=PURPLE))
-    
-    # Animate the transformation from original to contracted points
-    arrows = [Arrow(axes.c2p(point[0], point[1]), axes.c2p(contracted_point[0], contracted_point[1]), buff=0.1, color=WHITE)
-              for point, contracted_point in zip(points, contracted_points)]
-
     self.play(*[Create(contracted_dot) for contracted_dot in contracted_dots],
               *[Create(arrow) for arrow in arrows],
               *[Create(line) for line in contracted_lines])
     self.wait(2)
-
-    # Add a label to indicate contraction mapping
-    contraction_label = MathTex(r"f(x, y) = (\frac{1}{2}x, \frac{1}{2}y)")
-    contraction_label.to_corner(UP + LEFT)
-    self.play(Write(contraction_label))
+    self.next_slide()
 
     # Add a text description indicating that f is a contraction mapping
-    contraction_text = Text("f is a contraction mapping (shrinks distances)", font_size=24).to_edge(UP)
+    contraction_text = MathTex(r"f\text{ is called a contraction mapping.}") \
+        .to_edge(UP + RIGHT)
     self.play(Write(contraction_text))
 
     self.next_slide()
 
+    self.play(FadeOut(VGroup(*arrows)))
+    # Highlight the fixed point (0, 0)
+    fixed_point = Dot(axes.c2p(0, 0), color=BLUE).scale(1.5)
+    fixed_point_label = Text("Fixed Point", font_size=24).to_corner(DOWN + LEFT)
+    fixed_point_arrow = Arrow(fixed_point_label.get_boundary_point(UP), axes.c2p(0, 0), buff=0.1, color=WHITE)
+    self.play(Create(fixed_point), Write(fixed_point_label), Create(fixed_point_arrow))
+    self.wait(1)
+
+    self.next_slide()
+
     # Fade out all elements
-    self.play(FadeOut(VGroup(axes, labels, *dots, *contracted_dots, *arrows, *original_lines, *contracted_lines, contraction_label, fixed_point, fixed_point_label, contraction_text)))
+    self.play(FadeOut(VGroup(axes, labels, *dots, *contracted_dots, *original_lines, *contracted_lines, contraction_label, fixed_point, fixed_point_label, fixed_point_arrow, contraction_text)))
